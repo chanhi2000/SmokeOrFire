30c7bfe145931d90eeecb429e54d773275315da4
Justin Hester <justinlawrencehester@gmail.com>
2016-08-06 15:41:50 -0700

    Added initial Xcode project files.

---
+++ SmokeOrFire/SmokeOrFire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmokeOrFire
+//
+//  Created by Justin Lawrence Hester on 8/6/16.
+//  Copyright © 2016 Justin Lawrence Hester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
