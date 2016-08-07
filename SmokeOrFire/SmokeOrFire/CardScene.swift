d992f7d37a7293eaa655148d889db59b4d576ef2
chanhi2000 <chanhi2000@gmail.com>
2016-08-07 16:05:59 -0700

    images moved

---
+++ SmokeOrFire/SmokeOrFire/CardScene.swift
@@ -0,0 +1,13 @@
+//
+//  CardScene.swift
+//  SmokeOrFire
+//
+//  Created by LeeChan on 8/7/16.
+//  Copyright © 2016 Justin Lawrence Hester. All rights reserved.
+//
+
+import UIKit
+
+class CardScene: SKScene {
+
+}
