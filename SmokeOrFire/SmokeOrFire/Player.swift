6f81f08ed0c395e0d68d7ddf97ac9ce0967167f4
Justin Hester <justinlawrencehester@gmail.com>
2016-08-06 23:38:31 -0700

    Refactored Player and added PlayerChoices.

--- SmokeOrFire/SmokeOrFire/Player.swift
+++ SmokeOrFire/SmokeOrFire/Player.swift
@@ -9,9 +9,36 @@
 import UIKit
 
 class Player: NSObject {
+    var number: Int
     var hand: [Card] = [];
+    var choice: PlayerChoices? = nil
     
+    init(number: Int) {
+        self.number = number
+    }
+
     func addCard(card: Card) {
         hand.append(card)
     }
+
+    func setChoice(choice: PlayerChoices) {
+        self.choice = choice
+    }
+
+}
+
+enum PlayerChoices: UInt8 {
+    // Suit?
+    case HEART = 1, CLUB, DIAMOND, SPADE
+    // Smoke or fire?
+    case RED
+    case BLACK
+    // Higher or lower?
+    case HIGHER
+    case LOWER
+    // Inside or outside?
+    case INSIDE
+    case OUTSIDE
+    // Misc choice
+    case SAME
 }
---
+++ SmokeOrFire/SmokeOrFire/Round.swift
@@ -0,0 +1,9 @@
+//
+//  Round.swift
+//  SmokeOrFire
+//
+//  Created by Justin Lawrence Hester on 8/6/16.
+//  Copyright © 2016 Justin Lawrence Hester. All rights reserved.
+//
+
+import Foundation
