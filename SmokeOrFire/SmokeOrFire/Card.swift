8344657ca70ca315242e9f9350dc6cd98c7ace96
chanhi2000 <chanhi2000@gmail.com>
2016-08-06 20:09:03 -0700

    Added extensions and modified rank, suit, and card

--- SmokeOrFire/SmokeOrFire/Card.swift
+++ SmokeOrFire/SmokeOrFire/Card.swift
@@ -6,14 +6,11 @@
 //  Copyright © 2016 Justin Lawrence Hester. All rights reserved.
 //
 
-import UIKit
-
 struct Card {
-    // properties
     var rank: Rank
     var suit: Suit
     
-    // methods
-    func getRank() -> Rank {  return self.rank  }
-    func getSuit() -> Suit {  return self.suit  }
+    func describe() -> String {
+        return "The \(rank.describe()) of \(suit.describe())"
+    }
 }
