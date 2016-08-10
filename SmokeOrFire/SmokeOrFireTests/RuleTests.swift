8b752e0dcfba5233e5b99c7ca0866885b33253ba
Justin Hester <justinlawrencehester@gmail.com>
2016-08-10 04:05:24 -0700

    Added RuleTests.swift

---
+++ SmokeOrFire/SmokeOrFireTests/RuleTests.swift
@@ -0,0 +1,24 @@
+//
+//  RuleTests.swift
+//  SmokeOrFire
+//
+//  Created by Justin Lawrence Hester on 8/10/16.
+//  Copyright © 2016 Justin Lawrence Hester. All rights reserved.
+//
+
+@testable import SmokeOrFire
+import XCTest
+
+class RuleTests: XCTestCase {
+
+    func testTitle() {
+        XCTAssertEqual(Rule.COLOR.title(), "Smoke or Fire?")
+        XCTAssertEqual(Rule.UP_DOWN.title(), "UP or DOWN?")
+        XCTAssertEqual(Rule.IN_OUT.title(), "INSIDE or OUTSIDE?")
+        XCTAssertEqual(Rule.SUIT.title(), "What's the SUIT?")
+        XCTAssertEqual(Rule.POKER.title(), "Texax Hold'em!")
+        XCTAssertEqual(Rule.GIVE.title(), "")
+        XCTAssertEqual(Rule.TAKE.title(), "")
+    }
+
+}
