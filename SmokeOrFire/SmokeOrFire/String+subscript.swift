692ca8cb42f20e6f492efe96f6bce0bce62a6238
Justin Hester <justinlawrencehester@gmail.com>
2016-08-13 16:02:52 -0700

    Added String+subscript.swift

---
+++ SmokeOrFire/SmokeOrFire/String+subscript.swift
@@ -0,0 +1,9 @@
+//
+//  String+subscript.swift
+//  SmokeOrFire
+//
+//  Created by Justin Lawrence Hester on 8/13/16.
+//  Copyright © 2016 Justin Lawrence Hester. All rights reserved.
+//
+
+import Foundation
