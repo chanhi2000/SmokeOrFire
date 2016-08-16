c08a30693d7f6f26a763f59cc70ceb9e6e153c41
Justin Hester <justinlawrencehester@gmail.com>
2016-08-16 03:04:30 -0700

    Got rid of constraint errors with placeholders and finished game layout.

--- SmokeOrFire/SmokeOrFire/Views/StatusView.swift
+++ SmokeOrFire/SmokeOrFire/Views/StatusView.swift
@@ -10,21 +10,13 @@ import UIKit
 
 class StatusView: UIView {
 
-    // MARK: - Constant Variables
-
+    // Constant Variables
     private let WIDTH_UNITS = 18.0
     private let HEIGHT_UNITS = 8.0
 
     // MARK: - UI Variables
-
     var statusButton: UIButton!
     var statusLabel: UILabel!
-
-    // MARK: - Instance Variables
-
-    private var viewsDictionary: [String: AnyObject]!
-
-    // MARK: - UI View
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -34,38 +26,40 @@ class StatusView: UIView {
         // Setup status button.
         statusButton = UIButton(frame: CGRect(
             x: CGFloat(1.0 / WIDTH_UNITS) * frame.width,
-            y: CGFloat(1.0 / HEIGHT_UNITS) * frame.height,
+            y: CGFloat(1.5 / HEIGHT_UNITS) * frame.height,
             width: CGFloat(5.0 / WIDTH_UNITS) * frame.width,
-            height: CGFloat(6.0 / HEIGHT_UNITS) * frame.height))
+            height: CGFloat(5.5 / HEIGHT_UNITS) * frame.height))
         statusButton.enabled = false
-        statusButton.layer.cornerRadius = statusButton.frame.width / 2.5
+        statusButton.layer.cornerRadius = statusButton.frame.width / 2
         statusButton.backgroundColor = .whiteColor()
         statusButton.setTitle("P1", forState: .Normal)
         statusButton.setTitleColor(.blackColor(), forState: .Normal)
         statusButton.titleLabel!.textAlignment = .Center
         addSubview(statusButton)
+        // Apply status button constraints.
+        addConstraint(NSLayoutConstraint(item: statusButton, attribute: .Width,
+            relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
+            multiplier: 1.0, constant: statusButton.frame.width))
+        addConstraint(NSLayoutConstraint(item: statusButton, attribute: .Height,
+            relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
+            multiplier: 1.0, constant: statusButton.frame.height))
 
         // Setup status label.
         statusLabel = UILabel(frame: CGRect(
             x: CGFloat(7.0 / WIDTH_UNITS) * frame.width,
-            y: CGFloat(1.0 / HEIGHT_UNITS) * frame.height,
+            y: CGFloat(1.5 / HEIGHT_UNITS) * frame.height,
             width: CGFloat(10.0 / WIDTH_UNITS) * frame.width,
-            height: CGFloat(6.0 / HEIGHT_UNITS) * frame.height))
+            height: CGFloat(5.5 / HEIGHT_UNITS) * frame.height))
         statusLabel.textAlignment = .Center
         statusLabel.text = Rule.COLOR.title()
         addSubview(statusLabel)
-
-        viewsDictionary = ["statusButton": statusButton, "statusLabel": statusLabel]
-
-        // Apply horizontal constraints.
-        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:|[statusButton][statusLabel]|", options: [], metrics: nil, views: viewsDictionary))
-
-        // Apply vertical constraints.
-        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|[statusButton]|", options: [], metrics: nil, views: viewsDictionary))
-        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|[statusLabel]", options: [], metrics: nil, views: viewsDictionary))
+        // Apply status label constraints.
+        addConstraint(NSLayoutConstraint(item: statusLabel, attribute: .Width,
+            relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
+            multiplier: 1.0, constant: statusLabel.frame.width))
+        addConstraint(NSLayoutConstraint(item: statusLabel, attribute: .Height,
+            relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute,
+            multiplier: 1.0, constant: statusLabel.frame.height))
 
     }
 
