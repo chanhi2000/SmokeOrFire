ffab0b540daa77591424a2793ff9810481271f1a
Justin Hester <justinlawrencehester@gmail.com>
2016-08-16 04:35:42 -0700

    Completed TODO for refactoring ButtonView.setButtonsFor()

--- SmokeOrFire/SmokeOrFire/Views/ButtonView.swift
+++ SmokeOrFire/SmokeOrFire/Views/ButtonView.swift
@@ -43,117 +43,129 @@ class ButtonView: UIView {
         }
     }
 
-    // TODO: - Figure out how to refactor this method that minimizes redundancy.
-    // One idea is to separate the case code blocks into separate methods
-    // TODO: - Research if attributes can be set for a list of UIButtons.
+    func setButtonTitles(buttons: [UIButton!], choices: [String?]) {
+        for i in 0.stride(to: choices.count, by: 1) {
+            if (choices[i] != nil) {
+                buttons[i].setTitle(choices[i], forState: .Normal)
+            } else {
+                buttons[i].hidden = true
+            }
+        }
+    }
+
+    func setButtonFrames(buttons: [UIButton!], total: Int) {
+        switch (total) {
+        case 1:
+            // Fit one button.
+            buttons[0].layer.frame = CGRect(x: 0.0, y: 0.0,
+                width: frame.width, height: frame.height)
+            break
+        case 2:
+            // Fit two buttons.
+            buttons[0].layer.frame = CGRect(x: 0.0, y: 0.0,
+                width: 0.5 * frame.width, height: frame.height)
+            buttons[1].layer.frame = CGRect(x: frame.width / 2, y: 0.0,
+                width: frame.width / 2, height: frame.height)
+            break
+        case 3:
+            // Fit three buttons.
+            buttons[0].layer.frame = CGRect(x: 0.0, y: 0.0,
+                width: frame.width / 3.0, height: frame.height)
+            buttons[1].layer.frame = CGRect(x: 0.333 * frame.width, y: 0.0,
+                width: frame.width / 3.0, height: frame.height)
+            buttons[2].layer.frame = CGRect(
+                x: 0.666 * frame.width, y: 0.0,
+                width: frame.width / 3.0, height: frame.height)
+            break
+        case 4:
+            // Fit four buttons.
+            let buttonWidth = frame.width / 2.0
+            let buttonHeight = frame.height / 2.0
+
+            /*
+             * SUIT BUTTONS LAYOUT
+             *
+             * 1 | 3
+             * - . -
+             * 2 | 4
+             *
+             */
+            buttons[0].layer.frame = CGRect(
+                x: 0.0, y: 0.0,
+                width: buttonWidth, height: buttonHeight)
+            buttons[1].layer.frame = CGRect(
+                x: 0.0, y: frame.height / 2.0,
+                width: buttonWidth, height: buttonHeight)
+            buttons[2].layer.frame = CGRect(
+                x: frame.width / 2.0, y: 0.0,
+                width: buttonWidth, height: buttonHeight)
+            buttons[3].layer.frame = CGRect(
+                x: frame.width / 2.0, y: frame.height / 2.0,
+                width: buttonWidth, height: buttonHeight)
+            break
+        default:
+            break
+        }
+    }
+
     func setButtonsFor(round: Round) {
         dispatch_async(dispatch_get_main_queue()) { [weak self] in
             guard let strongSelf = self else { return }
 
-            // Make all buttons visible.
-            strongSelf.firstChoiceButton.hidden = false
-            strongSelf.secondChoiceButton.hidden = false
-            strongSelf.thirdChoiceButton.hidden = false
-            strongSelf.fourthChoiceButton.hidden = false
+            let buttons = [strongSelf.firstChoiceButton,
+                strongSelf.secondChoiceButton, strongSelf.thirdChoiceButton,
+                strongSelf.fourthChoiceButton]
 
-            // Set Autoresize on.
-            strongSelf.firstChoiceButton.translatesAutoresizingMaskIntoConstraints = true
-            strongSelf.secondChoiceButton.translatesAutoresizingMaskIntoConstraints = true
-            strongSelf.thirdChoiceButton.translatesAutoresizingMaskIntoConstraints = true
-            strongSelf.fourthChoiceButton.translatesAutoresizingMaskIntoConstraints = true
+            for button in buttons {
+                // Make all buttons visible.
+                button.hidden = false
+                // Set autoresize on.
+                button.translatesAutoresizingMaskIntoConstraints = true
+            }
 
             // Set button titles based on round rule.
             switch (round.rule) {
             case .COLOR:
                 // "Smoke or Fire?" choices: Black, Red
-                strongSelf.firstChoiceButton.setTitle(ChoicesText.BLACK.rawValue, forState: .Normal)
-                strongSelf.secondChoiceButton.setTitle(ChoicesText.RED.rawValue, forState: .Normal)
-                strongSelf.thirdChoiceButton.hidden = true
-                strongSelf.fourthChoiceButton.hidden = true
-
-                // Fit two buttons.
-                strongSelf.firstChoiceButton.layer.frame = CGRect(x: 0.0, y: 0.0,
-                    width: 0.5 * strongSelf.frame.width, height: strongSelf.frame.height)
-                strongSelf.secondChoiceButton.layer.frame = CGRect(x: strongSelf.frame.width / 2, y: 0.0,
-                    width: strongSelf.frame.width / 2, height: strongSelf.frame.height)
-
+                strongSelf.setButtonTitles(buttons,
+                    choices: [ChoicesText.BLACK.rawValue,
+                        ChoicesText.RED.rawValue, nil, nil])
+                strongSelf.setButtonFrames(buttons, total: 2)
                 break
             case .UP_DOWN:
                 // "Higher or lower?" choices: Higher, Same, Lower
-                strongSelf.firstChoiceButton.setTitle(ChoicesText.HIGHER.rawValue, forState: .Normal)
-                strongSelf.secondChoiceButton.setTitle(ChoicesText.SAME.rawValue, forState: .Normal)
-                strongSelf.thirdChoiceButton.setTitle(ChoicesText.LOWER.rawValue, forState: .Normal)
-                strongSelf.fourthChoiceButton.hidden = true
-
-                // Fit three buttons.
-                strongSelf.firstChoiceButton.layer.frame = CGRect(x: 0.0, y: 0.0,
-                    width: strongSelf.frame.width / 3.0, height: strongSelf.frame.height)
-                strongSelf.secondChoiceButton.layer.frame = CGRect(x: 0.333 * strongSelf.frame.width, y: 0.0,
-                    width: strongSelf.frame.width / 3.0, height: strongSelf.frame.height)
-                strongSelf.thirdChoiceButton.layer.frame = CGRect(x: 0.666 * strongSelf.frame.width, y: 0.0,
-                    width: strongSelf.frame.width / 3.0, height: strongSelf.frame.height)
-
+                strongSelf.setButtonTitles(buttons,
+                    choices: [ChoicesText.HIGHER.rawValue,
+                        ChoicesText.SAME.rawValue,
+                        ChoicesText.LOWER.rawValue, nil])
+                strongSelf.setButtonFrames(buttons, total: 3)
                 break
             case .IN_OUT:
                 // "Inside or outside?" choices: Inside, Same, Outside
-                strongSelf.firstChoiceButton.setTitle(ChoicesText.INSIDE.rawValue, forState: .Normal)
-                strongSelf.secondChoiceButton.setTitle(ChoicesText.SAME.rawValue, forState: .Normal)
-                strongSelf.thirdChoiceButton.setTitle(ChoicesText.OUTSIDE.rawValue, forState: .Normal)
-                strongSelf.fourthChoiceButton.hidden = true
-
-                // Fit three buttons.
-                strongSelf.firstChoiceButton.layer.frame = CGRect(x: 0.0, y: 0.0,
-                    width: strongSelf.frame.width / 3.0, height: strongSelf.frame.height)
-                strongSelf.secondChoiceButton.layer.frame = CGRect(x: 0.333 * strongSelf.frame.width, y: 0.0,
-                    width: strongSelf.frame.width / 3.0, height: strongSelf.frame.height)
-                strongSelf.thirdChoiceButton.layer.frame = CGRect(x: 0.666 * strongSelf.frame.width, y: 0.0,
-                    width: strongSelf.frame.width / 3.0, height: strongSelf.frame.height)
+                strongSelf.setButtonTitles(buttons,
+                    choices: [ChoicesText.INSIDE.rawValue, ChoicesText.SAME.rawValue,
+                        ChoicesText.OUTSIDE.rawValue, nil])
+                strongSelf.setButtonFrames(buttons, total: 3)
 
                 break
             case .SUIT:
                 // "Which suit?" choices: Club, Diamond, Heart, Spade
-                strongSelf.firstChoiceButton.setTitle(ChoicesText.CLUB.rawValue, forState: .Normal)
-                strongSelf.secondChoiceButton.setTitle(ChoicesText.DIAMOND.rawValue, forState: .Normal)
-                strongSelf.thirdChoiceButton.setTitle(ChoicesText.HEART.rawValue, forState: .Normal)
-                strongSelf.fourthChoiceButton.setTitle(ChoicesText.SPADE.rawValue, forState: .Normal)
-
-                // Fit four buttons in a grid.
-                let buttonWidth = strongSelf.frame.width / 2.0
-                let buttonHeight = strongSelf.frame.height / 2.0
-
-                /*
-                 * SUIT BUTTONS LAYOUT
-                 *
-                 * 1 | 3
-                 * - . -
-                 * 2 | 4
-                 *
-                */
-                strongSelf.firstChoiceButton.layer.frame = CGRect(x: 0.0, y: 0.0,
-                    width: buttonWidth, height: buttonHeight)
-                strongSelf.secondChoiceButton.layer.frame = CGRect(x: 0.0, y: strongSelf.frame.height / 2.0,
-                    width: buttonWidth, height: buttonHeight)
-                strongSelf.thirdChoiceButton.layer.frame = CGRect(x: strongSelf.frame.width / 2.0, y: 0.0,
-                    width: buttonWidth, height: buttonHeight)
-                strongSelf.fourthChoiceButton.layer.frame = CGRect(x: strongSelf.frame.width / 2.0, y: strongSelf.frame.height / 2.0,
-                    width: buttonWidth, height: buttonHeight)
-
+                strongSelf.setButtonTitles(buttons,
+                    choices: [ChoicesText.CLUB.rawValue, ChoicesText.DIAMOND.rawValue,
+                        ChoicesText.HEART.rawValue, ChoicesText.SPADE.rawValue])
+                strongSelf.setButtonFrames(buttons, total: 4)
                 break
             case .POKER:
                 // TODO - develop poker hand evaluator and poker results user interface.
                 break
             case .GIVE, .TAKE:
                 // "Smoke or Fire?" choices: Black, Red
-                strongSelf.firstChoiceButton.setTitle(ChoicesText.PYRAMID.rawValue, forState: .Normal)
-                strongSelf.secondChoiceButton.hidden = true
-                strongSelf.thirdChoiceButton.hidden = true
-                strongSelf.fourthChoiceButton.hidden = true
-
-                // Fit one button.
-                strongSelf.firstChoiceButton.layer.frame = CGRect(x: 0.0, y: 0.0,
-                    width: strongSelf.frame.width, height: strongSelf.frame.height)
+                strongSelf.setButtonTitles(buttons,
+                    choices: [ChoicesText.PYRAMID.rawValue, nil, nil, nil])
+                strongSelf.setButtonFrames(buttons, total: 1)
                 break
             }
         }
     }
+
 }
